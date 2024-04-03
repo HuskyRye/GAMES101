04a8f75d68609d3d3a35ca09230fe889ea69b284
HuskyRye <1412167533@qq.com>
2024-04-03 19:04:10 +0800

    clang-format on

--- Assignment2/代码框架/main.cpp
+++ Assignment2/代码框架/main.cpp
@@ -1,9 +1,8 @@
-// clang-format off
+#include "Triangle.hpp"
+#include "global.hpp"
+#include "rasterizer.hpp"
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include "rasterizer.hpp"
-#include "global.hpp"
-#include "Triangle.hpp"
 
 constexpr double MY_PI = 3.1415926;
 
@@ -12,12 +11,12 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
     Eigen::Matrix4f translate;
-    translate << 1,0,0,-eye_pos[0],
-                 0,1,0,-eye_pos[1],
-                 0,0,1,-eye_pos[2],
-                 0,0,0,1;
+    translate << 1, 0, 0, -eye_pos[0],
+        0, 1, 0, -eye_pos[1],
+        0, 0, 1, -eye_pos[2],
+        0, 0, 0, 1;
 
-    view = translate*view;
+    view = translate * view;
 
     return view;
 }
@@ -42,42 +41,37 @@ int main(int argc, const char** argv)
     bool command_line = false;
     std::string filename = "output.png";
 
-    if (argc == 2)
-    {
+    if (argc == 2) {
         command_line = true;
         filename = std::string(argv[1]);
     }
 
     rst::rasterizer r(700, 700);
 
-    Eigen::Vector3f eye_pos = {0,0,5};
+    Eigen::Vector3f eye_pos = { 0, 0, 5 };
 
+    std::vector<Eigen::Vector3f> pos {
+        { 2, 0, -2 },
+        { 0, 2, -2 },
+        { -2, 0, -2 },
+        { 3.5, -1, -5 },
+        { 2.5, 1.5, -5 },
+        { -1, 0.5, -5 }
+    };
 
-    std::vector<Eigen::Vector3f> pos
-            {
-                    {2, 0, -2},
-                    {0, 2, -2},
-                    {-2, 0, -2},
-                    {3.5, -1, -5},
-                    {2.5, 1.5, -5},
-                    {-1, 0.5, -5}
-            };
+    std::vector<Eigen::Vector3i> ind {
+        { 0, 1, 2 },
+        { 3, 4, 5 }
+    };
 
-    std::vector<Eigen::Vector3i> ind
-            {
-                    {0, 1, 2},
-                    {3, 4, 5}
-            };
-
-    std::vector<Eigen::Vector3f> cols
-            {
-                    {217.0, 238.0, 185.0},
-                    {217.0, 238.0, 185.0},
-                    {217.0, 238.0, 185.0},
-                    {185.0, 217.0, 238.0},
-                    {185.0, 217.0, 238.0},
-                    {185.0, 217.0, 238.0}
-            };
+    std::vector<Eigen::Vector3f> cols {
+        { 217.0, 238.0, 185.0 },
+        { 217.0, 238.0, 185.0 },
+        { 217.0, 238.0, 185.0 },
+        { 185.0, 217.0, 238.0 },
+        { 185.0, 217.0, 238.0 },
+        { 185.0, 217.0, 238.0 }
+    };
 
     auto pos_id = r.load_positions(pos);
     auto ind_id = r.load_indices(ind);
@@ -86,8 +80,7 @@ int main(int argc, const char** argv)
     int key = 0;
     int frame_count = 0;
 
-    if (command_line)
-    {
+    if (command_line) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
         r.set_model(get_model_matrix(angle));
@@ -104,8 +97,7 @@ int main(int argc, const char** argv)
         return 0;
     }
 
-    while(key != 27)
-    {
+    while (key != 27) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
         r.set_model(get_model_matrix(angle));
@@ -125,4 +117,3 @@ int main(int argc, const char** argv)
 
     return 0;
 }
-// clang-format on
