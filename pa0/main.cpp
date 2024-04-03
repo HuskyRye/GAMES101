215ccea53f2fec3f21519649c1fa14b971c7caff
HuskyRye <1412167533@qq.com>
2024-04-03 18:47:28 +0800

    Finish pa0.

--- pa0/main.cpp
+++ pa0/main.cpp
@@ -1,24 +1,24 @@
-#include<cmath>
-#include<eigen3/Eigen/Core>
-#include<eigen3/Eigen/Dense>
-#include<iostream>
+#include <cmath>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+#include <iostream>
 
-int main(){
-
+int main()
+{
     // Basic Example of cpp
     std::cout << "Example of cpp \n";
     float a = 1.0, b = 2.0;
     std::cout << a << std::endl;
-    std::cout << a/b << std::endl;
+    std::cout << a / b << std::endl;
     std::cout << std::sqrt(b) << std::endl;
     std::cout << std::acos(-1) << std::endl;
-    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
+    std::cout << std::sin(30.0 / 180.0 * acos(-1)) << std::endl;
 
     // Example of vector
     std::cout << "Example of vector \n";
     // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
+    Eigen::Vector3f v(1.0f, 2.0f, 3.0f);
+    Eigen::Vector3f w(1.0f, 0.0f, 0.0f);
     // vector output
     std::cout << "Example of output \n";
     std::cout << v << std::endl;
@@ -29,20 +29,40 @@ int main(){
     std::cout << "Example of scalar multiply \n";
     std::cout << v * 3.0f << std::endl;
     std::cout << 2.0f * v << std::endl;
+    // vector dot product
+    std::cout << "Example of dot product \n";
+    std::cout << v.dot(w) << std::endl;
 
     // Example of matrix
     std::cout << "Example of matrix \n";
     // matrix definition
-    Eigen::Matrix3f i,j;
+    Eigen::Matrix3f i, j;
     i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
     j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
     // matrix output
     std::cout << "Example of output \n";
     std::cout << i << std::endl;
     // matrix add i + j
+    std::cout << "Example of add \n";
+    std::cout << i + j << std::endl;
     // matrix scalar multiply i * 2.0
+    std::cout << "Example of scalar multiply \n";
+    std::cout << i * 2.0f << std::endl;
     // matrix multiply i * j
+    std::cout << "Example of matrix multiplication \n";
+    std::cout << i * j << std::endl;
     // matrix multiply vector i * v
+    std::cout << "Example of matrix vector multiplication \n";
+    std::cout << i * v << std::endl;
 
+    Eigen::Vector3f P(2, 1, 1);
+    std::cout << "Point P=(" << P(0) << ", " << P(1) << ")\n";
+    Eigen::Matrix3f transformation;
+    double theta = M_PI / 4;
+    transformation << cos(theta), -sin(theta), 1,
+        sin(theta), cos(theta), 2,
+        0, 0, 1;
+    P = transformation * P;
+    std::cout << "Transformed Point P=(" << P(0) << ", " << P(1) << ")\n";
     return 0;
 }
